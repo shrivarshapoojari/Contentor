f3000976ca20786965b3e0e4646ba8c4b1a71d37
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-09-20 16:16:37 +0530

    add  add content generation node

--- src/nodes/blog_node.py
+++ src/nodes/blog_node.py
@@ -24,3 +24,21 @@ class BlogNode:
             response=self.llm.invoke(system_message)
             
             return{"blog":{"title":response.content}}
+    
+
+    def content(self,state:BlogState):
+
+        if "topic" in state and  state["topic"]:
+            prompt="""You are an expert blog content writer.Use Markdown formatting to create a detailed and engaging blog post based on the given title.
+            Title:{topic}.
+            The blog post should be well-structured, informative, and captivating.
+            Ensure the content is unique, relevant to the title, and provides value to the readers.
+            Use headings, subheadings, bullet points, and other formatting elements to enhance readability.
+            Return only the blog content without any additional text or punctuation.
+          """
+            
+            system_message=prompt.format(topic=state["topic"])
+
+            response=self.llm.invoke(system_message)
+            
+            return{"blog":{"title":state["blog"]["title"],"content":response.content}}
